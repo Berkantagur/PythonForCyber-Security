f7b1dafa4a53bbc44339b0da617ece574830877a
Berkantagur <berkantagur6@gmail.com>
2023-03-18 18:04:48 +0300

    .py

--- fuzzing/fuzzing.py
+++ fuzzing/fuzzing.py
@@ -8,6 +8,7 @@ import requests
 file = open("fuzzing.txt", "r")
 content = file.read()
 file.close()
-
+header = {"Cookie": "scurity = low; PHPSESSID = ....................."}
 for i in content.split("\n"):
     print(i)
+    url = "http://10.10.10.10" + str(i)

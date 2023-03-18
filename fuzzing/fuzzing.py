c22d3af8ed1d695e42f0964093742b5094fd97bc
Berkantagur <berkantagur6@gmail.com>
2023-03-18 18:01:59 +0300

    .py

--- fuzzing/fuzzing.py
+++ fuzzing/fuzzing.py
@@ -2,3 +2,12 @@
 # technique that involves providing invalid, unexpected, or random data as inputs to a computer program.
 # In programming and software development, fuzzing or fuzz testing is an automated software testing 
 # technique that involves providing invalid, unexpected, or random data as inputs to a computer program.
+
+import requests
+
+file = open("fuzzing.txt", "r")
+content = file.read()
+file.close()
+
+for i in content.split("\n"):
+    print(i)

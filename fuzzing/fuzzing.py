f71ee9b876bcb216270259ac12668218dfa64eb0
Berkantagur <berkantagur6@gmail.com>
2023-03-18 17:55:44 +0300

    .py

---
+++ fuzzing/fuzzing.py
@@ -0,0 +1,4 @@
+# In programming and software development, fuzzing or fuzz testing is an automated software testing
+# technique that involves providing invalid, unexpected, or random data as inputs to a computer program.
+# In programming and software development, fuzzing or fuzz testing is an automated software testing 
+# technique that involves providing invalid, unexpected, or random data as inputs to a computer program.
